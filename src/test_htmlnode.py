d44d2f4642915d5ffdc9029c1f48756644e6ff8a
airst4r <dezoscream@gmail.com>
2025-05-07 09:57:36 +0300

    static site

---
+++ src/test_htmlnode.py
@@ -0,0 +1,33 @@
+import unittest
+
+from htmlnode import HTMLNode, LeafNode, ParentNode
+
+
+class TestHTMLNode(unittest.TestCase):
+    def test_eq(self):
+        node = HTMLNode("tag1", "value1", None, {"test1" : "value of test1", "addr" : "https://google.com"})
+        print(node.props_to_html())
+        print(node)
+
+class TestLeafNode(unittest.TestCase):
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+class TestParentNode(unittest.TestCase):
+    def test_to_html_with_children(self):
+        child_node = LeafNode("span", "child")
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(parent_node.to_html(), "<div><span>child</span></div>")
+
+    def test_to_html_with_grandchildren(self):
+        grandchild_node = LeafNode("b", "grandchild")
+        child_node = ParentNode("span", [grandchild_node])
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(
+            parent_node.to_html(),
+            "<div><span><b>grandchild</b></span></div>",
+        )
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ src/test_textnode.py
@@ -0,0 +1,48 @@
+import unittest
+
+from textnode import TextNode, TextType, text_node_to_html_node
+from inline_markdown import extract_markdown_images, extract_markdown_links, split_nodes_image, split_nodes_link
+
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        node3 = TextNode("This is a text node", TextType.ITALIC)
+        node4 = TextNode("Node 4",TextType.BOLD, "https://test.test")
+        self.assertEqual(node, node2)
+        self.assertNotEqual(node,node3)
+        self.assertNotEqual(node3, node4)
+
+class TestTextNodeToHTMLNode(unittest.TestCase):
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_image(self):
+        node = TextNode("This is an image", TextType.IMAGE, "https://www.boot.dev")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "img")
+        self.assertEqual(html_node.value, "")
+        self.assertEqual(
+            html_node.props,
+            {"src": "https://www.boot.dev", "alt": "This is an image"},
+        )
+
+    def test_bold(self):
+        node = TextNode("This is bold", TextType.BOLD)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "b")
+        self.assertEqual(html_node.value, "This is bold")
+
+    def test_extract_markdown_images(self):
+        matches = extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
+
+if __name__ == "__main__":
+    unittest.main()
